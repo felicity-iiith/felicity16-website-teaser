897a30b2286d03813385e3753a405fbe6a903580
Harshit Harchani <hharchani@gmail.com>
2015-10-21 05:37:31 +0530

    Updated 404 page

--- app/views/404.php
+++ app/views/404.php
@@ -1,2 +1,86 @@
-<h2>404</h2>
-<h1>Page not found</h1>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+  <title>404 Page not found | Felicity · IIIT-H</title>
+  <link rel="stylesheet" href="<?php echo base_url(); ?>css/normalize.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>css/teaser.css">
+  <style>
+  .share {
+    -webkit-filter: url("<?php echo base_url(); ?>img/goo-filter.svg#goo");
+    filter: url("<?php echo base_url(); ?>img/goo-filter.svg#goo");
+  }
+  </style>
+  <link href="<?php echo base_url(); ?>css/font-awesome/css/font-awesome.min.css" rel="stylesheet"/>
+  <link href="https://fonts.googleapis.com/css?family=Flamenco|Noto+Sans" rel="stylesheet" type="text/css">
+</head>
+<body class="error404" style="background-color: #444">
+  <div class="wrapper">
+    <div class="main-menu">
+      <div class="share">
+        <button class="share-toggle-button">
+          <i class="share-icon fa fa-bars"></i>
+        </button>
+        <ul class="share-items">
+          <li class="share-item">
+            <a
+            class="share-button internal-link"
+            href="<?php echo base_url(); ?>"
+            title="Home"><i class="share-icon fa fa-home"></i></a>
+          </li>
+          <li class="share-item">
+            <a
+            class="share-button internal-link"
+            href="<?php echo base_url(); ?>about/"
+            title="About"><i class="share-icon fa fa-info"></i></a>
+          </li>
+          <li class="share-item">
+            <a
+            href="<?php echo base_url(); ?>contact/"
+            class="share-button internal-link"
+            title="Contact Us"><i class="share-icon fa fa-envelope"></i></a>
+          </li>
+          <li class="share-item">
+            <a
+            class="share-button"
+            href="https://felicity.iiit.ac.in/15/"
+            title="Old website" target="_blank"><i class="share-icon fa fa-external-link"></i></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <aside>
+      <div class="lamp">
+        <div class="bulb"></div>
+        <img src="<?php echo base_url(); ?>img/lamp.png">
+      </div>
+      <div class="lamp">
+        <div class="bulb"></div>
+        <img src="<?php echo base_url(); ?>img/lamp.png">
+      </div>
+    </aside>
+    <main>
+      <section class="text-center" style="color: white;">
+        <h1>Error 404</h1>
+        <h2>Page not found!</h2>
+        <p class="text-center">Sorry, the page you were looking for was not found.</p>
+      </section>
+    </main>
+  </div>
+  <footer>
+    Copyright <i class="fa fa-copyright"></i> 2015-2016 Team Felicity, <a target="_blank" href="https://www.iiit.ac.in/">IIIT Hyderabad</a>
+    <a class="internal-link" href="<?php echo base_url(); ?>credits/">Credits</a>
+  </footer>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+	<script src="<?php echo base_url(); ?>js/TweenMax.min.js"></script>
+	<script src="<?php echo base_url(); ?>js/beta-menu.js"></script>
+  <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','lala');
+  lalala(lala);
+  </script>
+</body>
+</html>
