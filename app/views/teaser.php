8c5d66fb0edb362c24d64f9ae01d08b988e3238c
Harshit Harchani <hharchani@gmail.com>
2016-01-27 14:52:18 +0530

    Add local jquery

--- app/views/teaser.php
+++ app/views/teaser.php
@@ -194,6 +194,7 @@
     <a class="internal-link" href="<?php echo base_url(); ?>credits/">Credits</a>
   </footer>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="<?php echo base_url(); ?>js/jquery.min.js"><\/script>')</script>
 	<script src="<?php echo base_url(); ?>js/TweenMax.min.js"></script>
 	<script src="<?php echo base_url(); ?>js/beta-menu.js"></script>
   <script>
